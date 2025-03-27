9b4c11e5a31de78f340b7fdca3b31c7ee9ea3a55
Jasmol <jarora20@myseneca.ca>
2025-03-27 07:45:19 +0530

    Initial commit

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class X{
+    char arr;
+};
+class Y{
+    char arr;
+};
+class Z{
+    char arr;
+}
+
+int main()
+{
+    
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+// creating a constructor
+class complex{
+    complex(void);
+    
+};
+int main()
+{
